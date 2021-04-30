12529a682e22707585262b55561663eefee6b692
XiaoShan2016 <295971920@qq.com>
2021-04-30 22:54:55 +0800

    .

--- Kuwo.js
+++ Kuwo.js
@@ -1,3 +1,12 @@
+/*
+KuWo music unlock vip
+
+QX1.0.0:
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/295971920/QuantumultX/master/Kuwo.js
+
+MITM = vip1.kuwo.cn
+*/
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
